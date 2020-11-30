14f815411cf50ee90f7f05d62b473514f8d2dd71
lucas2020 <llucas17@ensign.edu>
2020-11-30 13:44:19 -0700

    Header

---
+++ other_scripts/SQLServerCodeHeader.sql
@@ -0,0 +1,19 @@
+/*****************************************************************************************************************
+NAME:    SQLServerCodeHeader.sql
+PURPOSE: To be used in clas projects
+MODIFICATION LOG:
+Ver      Date        Author        Description
+-----   ----------   -----------   -------------------------------------------------------------------------------
+1.0     11/30/2020   LLUCAS2020      1. Saved this header for Ensign IT240
+
+RUNTIME: 
+Approx. 1 min
+NOTES: This is going to be saved on my repository, so I can easily find it when I need to use the header again.
+
+These are the varioius Extract, Transform, and Load steps needed for the Example Data
+LICENSE: This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+ 
+******************************************************************************************************************/
