0f39c3e866fc62b0abb96cca114a5add2fe43f43
lucas2020 <llucas17@ensign.edu>
2020-12-09 18:53:59 -0700

    Updates

--- Tables/dbo.tblRegionDim.sql
+++ Tables/dbo.tblRegionDim.sql
@@ -1,3 +1,23 @@
+/*****************************************************************************************************************
+NAME:    dbo.tblRegionDim
+PURPOSE: Process for the creation of the tables
+MODIFICATION LOG:
+Ver      Date        Author        Description
+-----   ----------   -----------   -------------------------------------------------------------------------------
+1.0     11/30/2020   LLUCAS2020      1. Created table dbo.tblRegionDim
+
+RUNTIME: 
+Approx. 1 min
+NOTES: Saving the data on how the tables were created.
+
+These are the varioius Extract, Transform, and Load steps needed for the Example Data
+LICENSE: This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+ 
+******************************************************************************************************************/
+
 USE [DFNB2]
 GO
 
--- Views/dbo.v_AccountDim.sql
+++ Views/dbo.v_AccountDim.sql
@@ -1,3 +1,23 @@
+/*****************************************************************************************************************
+NAME:    dbo.v_AccountDim
+PURPOSE: Process for the creation of the views
+MODIFICATION LOG:
+Ver      Date        Author        Description
+-----   ----------   -----------   -------------------------------------------------------------------------------
+1.0     11/30/2020   LLUCAS2020      1. Created view dbo.v_AccountDim
+
+RUNTIME: 
+Approx. 1 min
+NOTES: Saving the data on how the views were created.
+
+These are the varioius Extract, Transform, and Load steps needed for the Example Data
+LICENSE: This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+ 
+******************************************************************************************************************/
+
 USE [DFNB2]
 GO
 
